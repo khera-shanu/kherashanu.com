9a7d5fd2d779b3831ef2c9691a28b1370d36edcf
khera-shanu <kherashanu@gmail.com>
2026-04-25 16:38:54 +0530

    clean slate

--- src/backend/src/mime.h
+++
@@ -1,8 +0,0 @@
-#ifndef MIME_H
-#define MIME_H
-
-// Get MIME type for file extension
-// Returns "application/octet-stream" for unknown types
-const char *mime_type_for_extension(const char *path);
-
-#endif // MIME_H
